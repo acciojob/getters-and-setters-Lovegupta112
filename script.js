639551168b13ad72f23938f0fc951e60acc2573c
lovegupta112 <loveguptaofficials@gmail.com>
2023-06-15 23:35:02 +0530

    added class functionality

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
 //complete this code
-class Person {}
+class Person {
+    age;
+    name;
+    constructor(name,age){
+      this.name=name;
+      this.age=age;
+    }
+    set setAge(age){
+        this.age=age;
+    }
+    get getName(){
+        return this.name;
+    }
+}
 
-class Student extends Person {}
+class Student extends Person {
+    constructor(name,age){
+        super(name,age);
+    }
+    study(){
+        console.log(`${this.name} is studying`);
+    }
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+    constructor(name,age){
+        super(name,age);
+    }
+    teach(){
+        console.log(`${this.name} is teaching`);
+    }
+}
+
+let student1=new Student('Love',21);
+console.log(student1);
+// student1.teach();
+student1.study();
+let teacher1=new Teacher('Max',30);
+teacher1.teach();
 
 // Do not change the code below this line
 window.Person = Person;
