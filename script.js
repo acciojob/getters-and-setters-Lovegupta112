3e9b9a5af0a6b36ca12798bfcacc80cca272185a
Love Gupta <90507983+Lovegupta112@users.noreply.github.com>
2023-06-17 19:31:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,43 +1,75 @@
 //complete this code
+// class Person {
+// 	_name;
+// 	_age;
+//     constructor(name,age){
+//       this._name=name;
+//       this._age=age;
+//     }
+//     set setAge(age){
+//         this._age=age;
+//     }
+//     get getName(){
+//         return this._name;
+//     }
+// }
+
+// class Student extends Person {
+//     constructor(name,age){
+//         super(name,age);
+//     }
+//     study(){
+//         console.log(`${this._name} is studying`);
+//     }
+// }
+
+// class Teacher extends Person {
+//     constructor(name,age){
+//         super(name,age);
+//     }
+//     teach(){
+//         console.log(`${this._name} is teaching`);
+//     }
+// }
+
+// let student1=new Student('Ramesh',21);
+// console.log(student1);
+// // student1.teach();
+// student1.study();
+// let teacher1=new Teacher('Max',30);
+// teacher1.teach();
+
 class Person {
-	_name;
-	_age;
-    constructor(name,age){
-      this._name=name;
-      this._age=age;
-    }
-    set setAge(age){
-        this._age=age;
-    }
-    get getName(){
-        return this._name;
-    }
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set age(age) {
+    this._age = age;
+  }
 }
 
 class Student extends Person {
-    constructor(name,age){
-        super(name,age);
-    }
-    study(){
-        console.log(`${this._name} is studying`);
-    }
+  study() {
+    console.log(`${this._name} is studying`);
+  }
 }
 
 class Teacher extends Person {
-    constructor(name,age){
-        super(name,age);
-    }
-    teach(){
-        console.log(`${this._name} is teaching`);
-    }
+  teach() {
+    console.log(`${this._name} is teaching`);
+  }
 }
 
-let student1=new Student('Ramesh',21);
-console.log(student1);
-// student1.teach();
-student1.study();
-let teacher1=new Teacher('Max',30);
-teacher1.teach();
+// Do not change the code below this line
+window.Person = Person;
+window.Student = Student;
+window.Teacher = Teacher;
 
 // Do not change the code below this line
 window.Person = Person;
